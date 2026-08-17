7412cfd9c70c2e8187dc36c512123bd0b3c3ac69
Brandon Omar Pineda Gomez <brandonomarpinedagomez7512004@gmail..com>
2026-08-17 16:36:15 -0600

    Migrar proyecto a MySQL

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -78,10 +78,16 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prueba_registros',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
